99295c3d827d3665e37e55ef4441a6d3e118dec5
Vlad AStahov <vlad-astahov@mail.ru>
2024-08-11 23:53:52 +0300

    init repo

---
+++ src/pages/Home.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+h1 HOME
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted } from "vue"; 
+
+export default defineComponent({
+    name: 'Home',
+})
+</script>
